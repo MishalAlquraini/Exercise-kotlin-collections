012d073578ede67cca0a71fec9399b8ad0cbd094
Mishal Alquraini <alqurainimishal@gmail.com>
2025-02-12 23:36:52 +0300

    done with part 2

--- task/src/Main.kt
+++ task/src/Main.kt
@@ -1,12 +1,13 @@
 
 
 val fruitMList = mutableListOf("Apple","Mango","Orange")
-
-
+val numSet = setOf(1, 2, 3, 3, 4, 5, 5)
+val addNumber = numSet + 6
 fun main() {
 
     fruitMList.add("Grapes")
     println(fruitMList)
     fruitMList.removeAt(1)
     println(fruitMList)
+    println(addNumber)
 }
